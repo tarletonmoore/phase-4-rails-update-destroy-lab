9bbeee4587a609bdcb0fbc84d0ef6a44921fa426
John Tarleton Moore <tarletonmoore@gmail.com>
2022-11-10 13:19:34 -0500

    commit tree 91711dab84f3ec500234eadb2de8b6499e37855e

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,7 +18,23 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
+
   private
+
+def find_plant
+Plant.find(params[:id])
+end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
